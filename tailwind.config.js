f8c1d53bc31172b2d7683bb5eeba23f6b04785bb
Nathan <nathanferreira00@hotmail.com>
2023-10-27 16:03:28 -0300

    feat: home page loading screen (skeleton)

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,13 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    fontFamily: {
-      sans: 'var(--font-inter)',
-    },
-    gridTemplateRows: {
-      app: 'min-content max-content',
+    extend: {
+      fontFamily: {
+        sans: 'var(--font-inter)',
+      },
+      gridTemplateRows: {
+        app: 'min-content max-content',
+      },
     },
   },
   plugins: [],
