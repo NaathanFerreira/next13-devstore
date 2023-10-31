210d456a99cf818e49e3a61e1e1c2e6a0c011fa1
Nathan <nathanferreira00@hotmail.com>
2023-10-31 16:14:03 -0300

    feat: adding products to cart

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import { AddToCartButton } from '@/components/AddToCartButton'
 import { api } from '@/data/api'
 import { Product } from '@/data/types/product'
 import { Metadata } from 'next'
@@ -115,12 +116,7 @@ export default async function ProductPage({ params }: ProductPageProps) {
           </div>
         </div>
 
-        <button
-          type="button"
-          className="mt-8 flex h-12 items-center justify-center rounded-full bg-emerald-600 font-semibold text-white"
-        >
-          Add to Cart
-        </button>
+        <AddToCartButton productId={product.id} />
       </div>
     </div>
   )
